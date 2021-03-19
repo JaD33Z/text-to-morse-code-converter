73bb5e0d1524877864c2fe591c69d419442ade8d
jonathandavis <jondavis8128@gmail.com>
2021-03-19 11:14:21 -0400

    first

---
+++ main.py
@@ -0,0 +1,35 @@
+#
+# ########## Convert Strings To Morse Code #######################
+
+def text_to_morse_code(text):
+    code_dict = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+    message_in_morse_code = ""
+
+    for letter in text:
+        if letter != ' ':
+            message_in_morse_code += code_dict[letter.upper()]
+        else:
+            message_in_morse_code += ' '
+    return message_in_morse_code
+
+
+text = input("Enter your message here: ")
+
+print(text_to_morse_code(text))
+
+
